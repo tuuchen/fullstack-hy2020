72699d85892e6aea56221c51073b3f1af5398b54
tuuchen <tuuchen@outlook.com>
2020-07-23 20:26:08 +0300

    part 4c

--- osa4/notebackend/controllers/notes.js
+++ osa4/notebackend/controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (req, res) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   res.json(notes.map((note) => note.toJSON()))
 })
 
@@ -14,6 +15,8 @@ notesRouter.get('/:id', async (req, res) => {
 notesRouter.post('/', async (req, res) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
   if (!body.content) {
     return res.status(400).json({
       error: 'content missing',
@@ -24,9 +27,12 @@ notesRouter.post('/', async (req, res) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   res.json(savedNote.toJSON())
 })
 
